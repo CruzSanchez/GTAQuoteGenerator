c6592f79e44f1e69cd14abe34207531cb08e9aaf
Cruz Sanchez <cruzsanchez9209@gmail.com>
2022-02-23 21:12:26 -0600

    Added Character class and Quote class with a string array with place holders for now.

---
+++ GTAQuoteGenerator/Models/Quote.cs
@@ -0,0 +1,7 @@
+﻿namespace GTAQuoteGenerator.Models
+{
+    public class Quote
+    {
+        public string[] Quotes { get; set; } = new string[2] { "THATS MY DAWG", "ICE COLD BABY ICE COLD" };
+    }
+}
